749c5462cf78488a10e6f34a499f27f938236c2d
saurav-joshi1999 <joshi199saurav@gmail.com>
2025-12-27 21:53:33 +0530

    remove the dotnet folder

--- obj/Debug/net9.0/ConsoleApp2.AssemblyInfo.cs
+++ obj/Debug/net9.0/ConsoleApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b74564e67fc60b68b10dd7ca2c7f26cb5d7c8a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d95115c4fc2cb743a40b08c2e89c814789c80538")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
